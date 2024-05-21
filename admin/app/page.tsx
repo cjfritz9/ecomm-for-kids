cab9536b4a3a4f2d3b1eb6d472096f64dba1b823
CJ <cjfritz9@gmail.com>
2024-05-21 17:20:50 -0400

    added homepage redirect logic for user with/without token

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,11 +1,15 @@
 import { Welcome } from '@/components/Welcome/Welcome';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
+import { getUserTokenData } from '@/lib/utils/cookies';
+import { getServerSideStoreData } from './api/utils';
+import { redirect } from 'next/navigation';
 
-export default function HomePage() {
-  return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
-  );
+export default async function HomePage() {
+  const token = getUserTokenData();
+
+  if (token) {
+    redirect('/dashboard')
+  } else {
+    redirect('/login')
+  }
 }
